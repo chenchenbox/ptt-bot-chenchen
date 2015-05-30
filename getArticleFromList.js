227dca074c237322b863c6c9838829099154225f
張家誠 <bird11419@yahoo.com.tw>
2015-05-30 18:56:14 +0800

    add get todays articles. ya!

---
+++ getArticleFromList.js
@@ -0,0 +1,72 @@
+var myBot = require('./PTT-BOT/ptt-bot');
+var fs = require('fs');
+var iconv = require('iconv-lite'); 
+var S = require('string');
+
+fs.readFile('targetArticleList.txt',{encoding:'utf-8'}, function (err, data) {
+	
+	articleList = JSON.parse(data);
+	var totalNum = articleList.length;
+
+	
+	//console.log( totalNum );
+	
+	//login
+	fs.readFile('myID.txt',{encoding:'utf-8'}, function (err, data) {
+	
+		if (err) throw err;
+		id = S(data).between("ID:'","'").s;
+		ps = S(data).between("PS:'","'").s;
+	
+		//console.log( articleList[0] );
+	
+		/*  與Ptt-sever建立連線  */
+		var conn = myBot.login(id,ps, function(){ //請自行輸入帳號密碼
+		
+			/*	登入完後即停留在主功能表 */	
+			console.log('已進入主功能表');
+		
+		});
+	
+		/*	進入欲收集的電影版版中	*/
+		myBot.toBoard('movie',function(){
+		
+			console.log('已進入movie板，接著收集文章!');
+		
+		});
+	
+	
+		/*	往後收集5篇文章	*/
+		for( var _=0;_<totalNum;_++ ){
+	
+			arrIndex = 0;
+			
+			/*	先進入文章中	*/
+			myBot.toArticle( articleList[_] ,function(){ 
+			
+				console.log('進入'+ articleList[arrIndex] +'文章中');
+				
+			});
+	
+			/*	接著下載文章	*/
+			myBot.loadArticle(function(){
+		
+				/*	從getArticle()取得文章內容	*/
+				fs.writeFile('./movie_data/'+'movie'+articleList[arrIndex]+'.txt', iconv.encode( myBot.escapeANSI( myBot.getArticle() ),'big5' ), function (err) {
+				
+					if (err) throw err;
+					console.log('movie'+articleList[arrIndex]+' 已經被儲存囉!');
+				
+					arrIndex++;
+				
+				});
+				
+			});
+		
+		}
+	
+	
+	});
+	
+	
+});
---
+++ getTodayList.js
@@ -0,0 +1,98 @@
+var myBot = require('./PTT-BOT/ptt-bot');
+var fs = require('fs');
+var iconv = require('iconv-lite'); 
+var S = require('string');
+
+
+
+//取得今天日期
+var dateObj = new Date();
+var thisMonth = dateObj.getMonth()+1;
+var thisDate = dateObj.getDate();
+var today = thisMonth.toString()+'/'+thisDate.toString();
+
+
+//login
+fs.readFile('myID.txt',{encoding:'utf-8'}, function (err, data) {
+	
+	if (err) throw err;
+	id = S(data).between("ID:'","'").s;
+	ps = S(data).between("PS:'","'").s;
+	
+	
+	
+	/*  與Ptt-sever建立連線  */
+	var conn = myBot.login(id,ps, function(){ //請自行輸入帳號密碼
+		
+			/*	登入完後即停留在主功能表 */	
+			console.log('已進入主功能表');
+		
+	});
+	
+	
+	var targetArticleIndex = [];
+		
+	
+	/*	進入欲收集的電影版版中	*/
+	myBot.toBoard('movie',function(){
+		
+		console.log('已進入movie板，接著收集文章!');
+		
+		//console.log(myBot.escapeANSI(myBot.getScreen()));
+		var screenData = S(myBot.getScreen()).replaceAll('●','  ').s;	
+		var screenArr =  S(myBot.escapeANSI(screenData)).lines();
+		//console.dir(screenArr);	
+		//console.log(S(screenArr[5]).right(-7).right(5).s);
+		//console.log(S(screenArr[5]).right(-16).right(4).s);
+		
+		for(var _ =0; _<screenArr.length;_++){
+			
+			var articleIndex = S(screenArr[_]).right(-7).right(5).s 
+			var articleDate = S(screenArr[_]).right(-16).right(4).s
+			if( articleDate == today){
+				targetArticleIndex.push(articleIndex);
+			}
+			
+		}
+		
+		//console.log(targetArticleIndex);
+		//console.log(targetArticleIndex.length);
+		
+	});
+	
+	
+	for(var i=0;i<=9;i++){
+		
+		myBot.sendPageUp(function(){
+			
+				var screenData = S(myBot.getScreen()).replaceAll('●','  ').s;
+				var screenArr =  S(myBot.escapeANSI(screenData)).lines();
+				//console.dir(screenArr);
+				//console.log(S(screenArr[5]).right(-7).right(5).s);
+				//console.log(S(screenArr[5]).right(-16).right(4).s);
+		
+				for(var _ =0; _<screenArr.length;_++){
+			
+					var articleIndex = S(screenArr[_]).right(-7).right(5).s 
+					var articleDate = S(screenArr[_]).right(-16).right(4).s
+					if( articleDate == today){
+						targetArticleIndex.push(articleIndex);
+					}
+				
+				}
+		
+				//console.log(targetArticleIndex);
+				//console.log(targetArticleIndex.length);
+		
+		
+				fs.writeFile('./targetArticleList.txt',JSON.stringify(targetArticleIndex), function (err) {
+					if (err) throw err;
+				});
+		
+			console.log("找尋List中.....");
+		
+		});	
+		
+	}
+
+});
