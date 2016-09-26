5f5e16852580f8a26970623c4bb808e6a5070097
張家誠 <bird11419@yahoo.com.tw>
2016-09-26 11:05:03 +0800

    update movieinfo.js

--- MovieInfo.js
+++ MovieInfo.js
@@ -2,7 +2,6 @@
 'use strict'
 
 const S = require('string')
-const fs = require('fs')//JUST FOR TEST
 const assert = require('assert')
 const cheerio = require('cheerio')
 const request = require('request')
@@ -27,18 +26,16 @@ var logger = new (winston.Logger)({
   ]
 })
 
-getTop20(()=>{})
-
 /**callback(err,top20info)**/
 function getTop20 (callback) {
-  assert.ok(typeof callback === 'function','Callback should be a funtion')
-  
+  assert.ok(typeof callback === 'function','callback should be a funtion')
+
   request('http://dorama.info/tw/drama/d_box_office.php',(err,res,html) => {
     if (!err && res.statusCode === 200){
       var $ = cheerio.load(html)
       var Top20 = []
       var _movieDOM = $('.table_g[width="99%"] tr').has('.table2_g[width="100%"] .td2_g[height=40]')
-  
+
       _movieDOM.each(function(i, ele){
         var cur = {}  
         
@@ -54,7 +51,7 @@ function getTop20 (callback) {
         } else {
           cur.mRank = null
           cur.mRanktype = null
-          logger.warn('%s is null while parsing movie page DOM','_rank')
+          logger.warn('In getTop20(), %s is null while parsing movie page DOM','_rank')
         }
 
         /** movie name and flim corporation **/
@@ -66,11 +63,12 @@ function getTop20 (callback) {
         }else {
           cur.mName = null
           cur.mFlimCor = null
-          logger.warn('%s is null while parsing movie page DOM','_nameANcor')
+          logger.warn('In getTop20(), %s is null while parsing movie page DOM','_nameANcor')
         }
 
-        var _new = $(this).find('.td2_g[height=40] font[color="#FF9900"]')
-        cur.mIsNew = (_new.length===1)
+        var _newFF = $(this).find('.td2_g[height=40] font[color="#FF9900"]')
+        var _new00 = $(this).find('.td2_g[height=40] font[color="#00CC00"]')
+        cur.mIsNew = (_newFF.length===1 || _new00.length===1)
         ? true
         : false
         
@@ -82,39 +80,82 @@ function getTop20 (callback) {
         } else {
           cur.mID = null
           cur.mLink = null
-          logger.warn('%s is null while parsing movie page DOM','_idANlink')
+          logger.warn('In getTop20(), %s is null while parsing movie page DOM','_idANlink')
         }
-        
+
         var _boxOffice = $(this).find('.td_dt_g[width=115]').text()
         if (_boxOffice) cur.mboxOffice = _boxOffice
         else {
           cur.mboxOffice = null
-          logger.warn('%s is null while parsing movie page DOM','_boxOffice')
+          logger.warn('In getTop20(), %s is null while parsing movie page DOM','_boxOffice')
         }
-        
+
         cur.mCast = []
         var _cast = $(this).find('.td2_g a[target="NEWWIN"]')
         _cast.each(function(){
           cur.mCast.push({
             actName: $(this).text(),
+            actID: S($(this).attr('href')).between('pf-','.html').s,
             actLink: 'http://dorama.info'+$(this).attr('href')
           })
           
         })
-
         Top20.push(cur)
       })
-
-      console.log(JSON.stringify(Top20))
-      callback(null,'top20info')
+      callback(null,JSON.stringify(Top20))
     }else if(err){
-      logger.error('Error has been throw out in request.js, %s', err)
+      logger.error('In getTop20(), err was thrown from the request.js, %s', err)
       callback(err,null)
     }else {
-      logger.error('Request did not act normally, the res.statusCode is %s', res.statusCode)
+      logger.error('In getTop20(), request did not act normally, the res.statusCode is %s', res.statusCode)
       callback(new Error('Request did not act normally, view ..logs/MovieInfo.log'),null)
     }
   })
 }
 
+getDetails('180275',(err)=>{if (err) throw err})
+//mID = mid or link
+function getDetails(mID, callback){
+  // var mid = 'http://dorama.info/drama-18027.html'
+  // var mid = '18027'
+  assert.ok(typeof mID === 'string','mID should be a string')
+  assert.ok(typeof callback === 'function','callback should be a funtion')
+  
+  var _Link = (S(mID).contains('http://dorama.info/drama')) 
+  ? mID
+  : 'http://dorama.info/drama-' + mID + '.html'
+
+  request(_Link, (err, res, html)=>{
+    if(!err && res.statusCode === 200) {
+      var $ = cheerio.load(html)
+      if (S($('body').text()).contains('【錯誤訊息中心】')){
+        if (S($('body').text()).contains('沒有這個檔案!!!')){
+          logger.error('In getDetails(), make sure the movie link or id (%s) is valid', mID)
+          callback(new Error('cant not find the document, plz make sure the movie link or id is valid'),null)
+        }else{
+          logger.error('In getDetails(), err was thrown from the website: %s', $('body').text())
+          callback(new Error('err was thrown from the website: '+$('body').text()),null)
+        }
+      }
+
+      /****code***/
+
+    } else if(err){
+      logger.error('In getDetails(), err was thrown from request.js: %s', err)
+      callback(err,null)
+    }else {
+      logger.error('In getDetails(), request did not act normally, the res.statusCode is %s', res.statusCode)
+      callback(new Error('Request did not act normally, view ..logs/MovieInfo.log'),null)
+    }
+    
+    /*  var $ = cheerio.load(html)
+      console.log(res.statusCode)
+      console.log($('body').text())
+      logger.error('Err occured during fetch html pages (%s) in getDetails, err contents: %s', _Link, $('body').text())
+     */
+  })
+
+}
+
 exports.getTop20 = getTop20
+exports.getDetails = getDetails
